665281c391751334fdfe82815bf9d58d81118fb7
ragul_ryan <ragul.n@disprz.com>
2025-09-16 22:31:53 +0530

    16 sep 10.30 pm

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347019f9958ad1b1ac6cd0aea1a9417311c4974d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527957df3e4a0c5eee7d0e0aed14dea5aa97d579")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
